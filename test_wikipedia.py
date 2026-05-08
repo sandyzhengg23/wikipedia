9a5ca960cd81600276a323fe9c41be3257522212
Sandy Zheng <128326330+sandyzhengg23@users.noreply.github.com>
2026-05-08 16:28:22 -0500

    Use regex for URL matching in Wikipedia test

--- test_wikipedia.py
+++ test_wikipedia.py
@@ -1,3 +1,4 @@
+import re
 from playwright.sync_api import Page, expect
 
 
@@ -7,5 +8,5 @@ def test_user_can_search_wikipedia_and_open_article(page: Page):
     page.get_by_role("searchbox").fill("University of Chicago")
     page.keyboard.press("Enter")
 
-    expect(page).to_have_url(lambda url: "University_of_Chicago" in url)
+    expect(page).to_have_url(re.compile(".*University_of_Chicago.*"))
     expect(page.get_by_role("heading", name="University of Chicago")).to_be_visible()
